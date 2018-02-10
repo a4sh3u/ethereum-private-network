17c5b64ec882d4c55a4bc324d8298c516ee9e4f0
Ashutosh Singh <ashutosh.singh@ubitricity.com>
2018-02-11 00:04:18 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+parity --chain chain-spec.json
+       --config config.toml
+       --bootnodes
+
+
+
+
+* in config.toml change chain=/parity/config/chain-spec.json
---
+++ create_network.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+command_exists() {
+	command -v "$@" > /dev/null 2>&1
+}
+user="$(id -un 2>/dev/null || true)"
+sh_c='sh -c'
+if [ "$user" != 'root' ]; then
+  if command_exists sudo; then
+    sh_c='sudo -E sh -c'
+  elif command_exists su; then
+    sh_c='su -c'
+  else
+    cat >&2 <<-'EOF'
+    Error: this installer needs the ability to run commands as root.
+    We are unable to find either "sudo" or "su" available to make this happen.
+    EOF
+    exit 1
+  fi
+fi
+
+
+
+docker run --name parity -v .:/parity/config parity/parity:stable --config=/parity/config/config.toml
