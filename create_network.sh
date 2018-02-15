fd84496b9952aedb98a7ec8061d45fa6cdb437db
Ashutosh Singh <ashutosh.singh@ubitricity.com>
2018-02-15 18:40:14 +0100

    all working - nodes connecting

--- create_network.sh
+++ create_network.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 set -e
+# Bootnode
+bootnode -nodekey boot.key -nat extip:192.168.178.47 -verbosity 9
 
-docker build -t epn/bootnode:latest ./bootnode/
-docker run -itd --name bootnode -e my_ip=$my_ip -v $(pwd)/bootnode/boot.key:/root/boot.key -p 30301:30301 -p 30301:30301/udp \
-epn/bootnode:latest --nodekey /root/boot.key --addr :30301
-# Extend the above command to use a external IP   --nat extip:$my_ip
+# Parity Node 1
+export password=12345
+export address=0xbc6c11b0d2db2bce3c52a97fbd28b342ae6bc0ae
+parity --chain chain-spec.json --author ${address} --unlock ${address} --password .parity-pass --base-path=. \
+--force-ui --rpccorsdomain "*" --jsonrpc-interface all --jsonrpc-hosts all >&1 1>>parity.log 2>&1 &
 
-docker run -v /root/ethereum-private-network:/parity/config parity/parity:stable --config=/parity/config/config.toml
-docker run -v /root/ethereum-private-network/client:/parity/config parity/parity:stable --config=/parity/config/config.toml
+
+
+parity --chain chain-spec.json --base-path=. --force-ui --rpccorsdomain "*" --jsonrpc-interface all --jsonrpc-hosts all \
+--bootnodes enode://a94c2edb40d6e4fcb017a32498c65387f0b6f2f5b4d603648747f9748d9e0663d4d688d2394e9c1ae6aa1bece6a7eb57c256e20f5e1b030aae4e03b4bf424883@192.168.178.132:30303 >&1 1>>parity.log 2>&1 &
